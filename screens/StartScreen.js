ce847fe9f3893f915a0786461cf9efe24dc47e3b
lh <lh@dievima.at>
2024-05-31 14:39:36 +0200

    styling

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native";
+
 import PrimaryButton from "../components/PrimaryButton";
+import Colors from "../constants/colors";
 
 function StartScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
@@ -61,7 +63,7 @@ const styles = StyleSheet.create({
     padding: 16,
     marginTop: 100,
     marginHorizontal: 24,
-    backgroundColor: "#3b021f",
+    backgroundColor: Colors.primary800,
     borderRadius: 8,
     elevation: 4,
     shadowColor: "#000",
@@ -73,9 +75,9 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     fontSize: 32,
-    borderBottomColor: "#ddb52f",
+    borderBottomColor: Colors.accent500,
     borderBottomWidth: 2,
-    color: "#ddb52f",
+    color: Colors.accent500,
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: "center",
